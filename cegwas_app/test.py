ff4dcc390c900a4f82d3264beb354ebc59b4e5d0
jpr71 <joshuapr1@gmail.com>
2015-11-18 17:16:38 -0600

    Added header

--- cegwas_app/test.py
+++ cegwas_app/test.py
@@ -1,14 +1,14 @@
-from app import app
+# from app import app
 
-import os
-import unittest
+# import os
+# import unittest
 
-class AppTestCase(unittest.TestCase):
+# class AppTestCase(unittest.TestCase):
 
-   def test_root_text(self):
-        tester = app.test_client(self)
-        response = tester.get('/')
-        assert 'Hello world!' in response.data
+#    def test_root_text(self):
+#         tester = app.test_client(self)
+#         response = tester.get('/')
+#         assert 'Hello world!' in response.data
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
