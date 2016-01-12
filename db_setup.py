24d0098e3db95101bd00e6abd567350fb65e5751
jpr71 <joshuapr1@gmail.com>
2016-01-12 16:49:57 -0600

    Populates Posgres database

--- db_setup.py
+++ db_setup.py
@@ -1,43 +1,52 @@
-import boto3
-import codecs
-import csv
 from collections import OrderedDict
-import os
+from peewee import *
 import re
 import requests
 
+db = PostgresqlDatabase(
+  'andersen',
+  user = 'andersen',
+  password = 'kniB3duE4drYil6hinD2yeD8juth8coUn2ef7Ayb',
+  host = 'andersen-db.cu9qvcawtap6.us-east-1.rds.amazonaws.com'
+  )
 
-dynamodb = boto3.resource('dynamodb')
+db.connect()
 
-table = dynamodb.create_table(
-    AttributeDefinitions = [
-    {
-      'AttributeName': 'Strain',
-      'AttributeType': 'S'
-    }
-  ],
-  TableName = 'strains',
-  KeySchema = [
-    {
-      'AttributeName': 'Strain',
-      'KeyType': 'HASH'
-    }
-  ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-)
 
-print table.does_table_exist()
 
-# print table.meta.client.get_waiter('table_exists')
+class strain(Model):
+  """C Elegans strain complete info database"""
+  strain = CharField()
+  isotype = CharField(null=True)
+  longitude = FloatField(null = True)
+  latitude = FloatField(null = True)
+  isolation = CharField(null=True)
+  location = CharField(null=True)
+  prev_names = CharField(null=True)
+  warning_msg =  CharField(null=True)
+  sequenced = BooleanField()
 
-# r = requests.get("https://raw.githubusercontent.com/AndersenLab/Andersen-Lab-Strains/master/processed/strain_info.tsv")
+  class Meta:
+    database = db
 
-# lines = r.text.splitlines()
 
-# for line in lines:
-#   print re.split('\t',line)
-# # print re.split('\t', r.text.splitlines())
-# # print r.text.decode("utf-8")
+db.drop_tables([strain], safe=True)
+db.create_tables([strain])
+
+r = requests.get("https://raw.githubusercontent.com/AndersenLab/Andersen-Lab-Strains/master/processed/strain_info_join.tsv")
+
+lines = r.text.splitlines()
+
+
+header = ["strain", "isotype", "longitude", "latitude", "isolation", "location", "prev_names", 
+"warning_msg", "sequenced"]
+
+for line in lines[1:]:
+  strain_info = re.split('\t',line)
+  data = OrderedDict(zip(header,strain_info))
+  data = {k:v for k,v in data.items()}
+  for k in data.keys():
+    if data[k] == "NA":
+      data[k] = None
+  strain(**data).save()
+ 
