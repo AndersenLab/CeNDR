471572bff1fec76099c6549f710a3160829d111e
Dan Cook <danielecook@gmail.com>
2016-01-13 13:42:07 -0600

    add models to db page; not sure why its not working if it is not still.

--- cegwas_app/models.py
+++ cegwas_app/models.py
@@ -1,5 +1,6 @@
 from peewee import *
 import json
+import datetime
 credentials = json.loads(open("../credentials.json",'r').read())
 
 db = PostgresqlDatabase(
@@ -10,16 +11,83 @@ db = PostgresqlDatabase(
 db.connect()
 
 class strain(Model):
-  """C Elegans strain complete info database"""
-  strain = CharField(index = True)
-  isotype = CharField(null=True, index = True)
-  longitude = FloatField(null = True)
-  latitude = FloatField(null = True)
-  isolation = CharField(null=True)
-  location = CharField(null=True)
-  prev_names = CharField(null=True)
-  warning_msg =  CharField(null=True)
-  sequenced = BooleanField()
+    """
+        C. Elegans strain information database
+    """
+    strain = CharField(index=True)
+    isotype = CharField(null=True, index=True)
+    longitude = FloatField(null=True)
+    latitude = FloatField(null=True)
+    isolation = CharField(null=True)
+    location = CharField(null=True)
+    prev_names = CharField(null=True)
+    warning_msg = CharField(null=True)
+    sequenced = BooleanField()
 
-  class Meta:
-    database = db
+    class Meta:
+        database = db
+
+
+class report(Model):
+    """
+        Reports
+    """
+    release = IntegerField(choices=((0, "public"), (1, "embargo6"), (2, "embargo12"), (3, "private")))
+    report_name = CharField(index=True, max_length=50, unique=True)
+    report_slug = CharField(index=True)
+    email = CharField(index=True)
+    submission_date = DateTimeField(default=datetime.datetime.now)
+    submission_complete = DateTimeField()
+    version = IntegerField(choices=((0, "report 1.0")))  # Version of Report
+
+    class Meta:
+        database = db
+
+
+class snp(Model):
+    """
+        Table of strain genotypes
+    """
+    strain = ForeignKeyField(strain)
+    chrom = CharField(index=True)
+    pos = IntegerField(index=True)
+    allele = IntegerField()
+    allele_base = CharField()
+    version = IntegerField(choices=((0, "snps 2.0")))  # Version of Snpset
+
+    class Meta:
+        database = db
+
+
+class trait(Model):
+    """ 
+        Initially, trait data only contains:
+          -report
+          -strain
+          -name (trait name)
+          -value (value of trait)
+    """
+    report = ForeignKeyField(report)
+    strain = ForeignKeyField(strain)
+    name = CharField(index=True)
+    value = DecimalField()
+
+    class Meta:
+        database = db
+
+
+class mapping(Model):
+
+    """ Results of mappings. Unique on peak IDs and markers. """
+    trait = ForeignKeyField(trait)
+    snp = ForeignKeyField(snp)
+    variance_explained = DecimalField()
+    log10p = DecimalField()
+    BF = DecimalField()
+    significant = BooleanField()
+    interval_start = IntegerField()
+    interval_end = IntegerField()
+
+    class Meta:
+        database = db
+
