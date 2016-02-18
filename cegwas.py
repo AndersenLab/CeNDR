21234a2c17ae239422090971964daf628e60aeb3
Daniel Cook <danielecook@gmail.com>
2016-02-18 09:10:20 -0600

    fix date for current variant set

--- cegwas.py
+++ cegwas.py
@@ -96,6 +96,7 @@ def map_page():
 def data_page():
     bcs = OrderedDict([("data", None)])
     title = "Data"
+    current_variant_set = "20160106"
     strain_listing = strain.select().filter(strain.isotype != None).order_by(strain.isotype).execute()
     return render_template('data.html', **locals())
 
