fb99df5c407a5a6a38b6789f39b8e53bf1a17333
Daniel Cook <danielecook@gmail.com>
2016-02-25 01:46:37 -0600

    add separate py script for emails [helps with spacing]

---
+++ emails.py
@@ -0,0 +1,9 @@
+mapping_submission = """
+You have submitted a genome-wide association mapping to <a href="http://www.elegansvariation.org">CeNDR</a> successfully.
+You can monitor progress of your report here:
+
+http://www.elegansvariation.org/report/{report_slug}/
+
+If you set your record to private, this email serves as a way for you to find the URL to your report. 
+
+"""
