ce78fa215f51ed9ab50c664afa1854a0d08e6cbf
Daniel Cook <danielecook@gmail.com>
2016-04-07 09:29:30 -0500

    fix map

--- cendr/__init__.py
+++ cendr/__init__.py
@@ -40,9 +40,8 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 if os.getenv('SERVER_SOFTWARE') and \
         os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/'):
     app.debug = False
-    print dir(app)
     from flask_sslify import SSLify
-    sslify = SSLify(app)
+    sslify = SSLify(app, skips=['strains/global-strain-map'])
 else:
     app.debug = True
     app.config['SECRET_KEY'] = "test"
