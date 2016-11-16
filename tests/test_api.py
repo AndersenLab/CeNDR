a427e587e296039eacfd67b1f23f7b9cadf07331
Dan Cook <danielecook@gmail.com>
2016-11-16 10:22:17 -0600

    add vendor

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,8 +4,13 @@ print sys.path
 import glob
 print glob.glob("*")
 print glob.glob("lib/*")
+print glob.glob("lib/*")
 
+from google.appengine.ext import vendor
+import os
 
+# Add any libraries installed in the "lib" folder.
+vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 from cendr.views.api import *
 
 
