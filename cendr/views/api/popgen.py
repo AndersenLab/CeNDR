682af2c822e5aa302cc26ebe6b714844e8367372
Daniel Cook <danielecook@gmail.com>
2017-06-18 21:47:57 -0500

    Update tajima to function with files

--- cendr/views/api/popgen.py
+++ cendr/views/api/popgen.py
@@ -1,20 +1,14 @@
-from flask_restful import Resource
-from cendr.models import tajimaD
 from flask import jsonify
-from cendr import api, cache
+from cendr import app, releases, autoconvert
+from subprocess import Popen, PIPE
 
 
-class tajima_d(Resource):
-    @cache.memoize(50)
-    def get(self, chrom, start, end):
-        data = list(tajimaD.select(tajimaD.BIN_START, tajimaD.TajimaD)
-                           .filter((tajimaD.CHROM == chrom) &
-                                   (tajimaD.BIN_START >= start - 50000) &
-                                   (tajimaD.BIN_END <= end + 50000),
-                                   ).tuples().execute())
-        data = [(int(x[0]) + 50000, float(x[1])) for x in data]
-        response = {"x": [x[0] for x in data], "y": [x[1] for x in data]}
-        return jsonify(response)
-
-api.add_resource(
-    tajima_d, '/api/variant/tajima/<string:chrom>/<int:start>/<int:end>')
+@app.route('/api/tajima/<string:chrom>/<int:start>/<int:end>')
+def tajima(chrom, start, end):
+    url = "http://storage.googleapis.com/elegansvariation.org/releases/{release}/tajima/WI.{release}.tajima.bed.gz".format(release = releases[0])
+    comm =['tabix', url, "{chrom}:{start}-{end}".format(**locals())]
+    out, err = Popen(comm, stdout = PIPE, stderr = PIPE).communicate()
+    out = [map(autoconvert, x.split("\t")) for x in out.splitlines()]
+    data = [(int(x[2]) + 50000, float(x[5])) for x in out]
+    response = {"x": [x[0] for x in data], "y": [x[1] for x in data]}
+    return jsonify(response)
