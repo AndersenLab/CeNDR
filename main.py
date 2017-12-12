efa877cba83682e1cee2bede0f68ba8c415f1875
Dan Cook <danielecook@gmail.com>
2017-12-11 18:28:26 -0600

    Add exception

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from cendr import app
 
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
