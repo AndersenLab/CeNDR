5cb5751d53a72869377e798789e58e3eaa97bb6f
Daniel Cook <danielecook@gmail.com>
2018-02-21 21:20:45 -0600

    update user

--- base/views/user.py
+++ base/views/user.py
@@ -6,7 +6,7 @@ Author: Daniel E. Cook
 User profile
 
 """
-from flask import render_template, Blueprint, session
+from flask import render_template, Blueprint, session, redirect, url_for, flash
 from base.models2 import user_m
 from logzero import logger
 
@@ -21,6 +21,9 @@ def user(username):
         The User Profile
     """
     user_obj = session.get('user')
+    if user_obj is None:
+        flash("You must be logged in to view that", 'danger')
+        return redirect(url_for('primary.primary'))
     VARS = {'title': username,
             'user_obj': user_m(user_obj['name'])}
     return render_template('user.html', **VARS)
