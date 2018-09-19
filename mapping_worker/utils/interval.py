b976c3c181836a5a7b1d6f46fc34b41209d99d11
Daniel Cook <danielecook@gmail.com>
2018-09-19 13:14:44 +0200

    update pipeline

--- mapping_worker/utils/interval.py
+++ mapping_worker/utils/interval.py
@@ -14,6 +14,7 @@ from logzero import logger
 
 DATASET_RELEASE = os.environ['DATASET_RELEASE']
 
+
 def process_interval(interval):
     """
         Processes an interval - producing a JSON summary in the data folder.
@@ -22,6 +23,7 @@ def process_interval(interval):
     # Download the VCF
     interval_fname = interval.replace(":", "-")
     interval_out = interval_fname + ".vcf.gz"
+    variants_out = interval_fname + ".variants.tsv.gz"
 
     # Subset VCF by isotypes
     df = pd.read_csv("df.tsv", sep='\t')
@@ -30,7 +32,15 @@ def process_interval(interval):
         comm = f"bcftools view -O z --samples {isotype_list} https://storage.googleapis.com/elegansvariation.org/releases/{DATASET_RELEASE}/variation/WI.{DATASET_RELEASE}.soft-filter.vcf.gz {interval} > {interval_out} && bcftools index {interval_out}"
         out, err = Popen(comm, shell=True).communicate()
     vcf = VCF_DataFrame.from_vcf(interval_out, interval)
-    return vcf.hard_filter() \
+
+    # Join interval variants here
+    interval_variants = pd.read_csv("data/interval_variants.tsv.gz", sep="\t")
+
+    # Prune VCF
+    vcf = vcf.hard_filter() \
               ._prune_alleles() \
-              ._prune_non_snps() \
-              .interval_summary_table()
+              ._prune_non_snps()
+
+    # Full join
+    vcf.to_csv(variants_out)
+    return vcf.interval_summary_table()
