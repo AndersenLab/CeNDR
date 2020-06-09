9438253398556bbb41a01ef944e18a4843202799
Daniel E. Cook <danielecook@gmail.com>
2020-06-09 08:29:24 -0400

    restruct data table

--- base/views/check.py
+++ base/views/check.py
@@ -5,11 +5,13 @@ from flask import jsonify, Blueprint
 check_bp = Blueprint('check',
                      __name__)
 
+
 @check_bp.route('/readiness_check')
 def readiness_check():
     response = jsonify({'ready': True})
     response.status_code = 200
     return response
+
 
 @check_bp.route('/liveliness_check')
 def liveliness_check():
